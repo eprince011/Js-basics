f2188eb1ed8a24066888809304675002d474f698
eprince001 <eprincewilliams@gmail.com>
2022-10-07 15:06:21 +0100

    Update test.js

--- test.js
+++ test.js
@@ -47,15 +47,26 @@ console.log(add(20, 20, 20))
 // }
 
 // create a loop that counts from 0 => 1000 -> index is 1, 2, 3, 4 etc. if the index is at 500, print a statement that says i'm about to break
-for (let index = 0; index <= 1000; index++) {
-    if (index == 500) {
-        console.log( "I'm going to break " + index);
-        break
-    }else {
-        console.log("Index" + " " + index);
-    }
-}
+// for (let index = 0; index <= 1000; index++) {
+//     if (index == 500) {
+//         console.log( "I'm going to break " + index);
+//         break
+//     }else {
+//         console.log("Index" + " " + index);
+//     }
+// }
 
 
+// const person = {
+//     firstName:"John",
+//     lastName: "Doe",
+//     fullName: function () {
+//       return this.firstName + " " + this.lastName;
+//     }
+//   }
+  
+//   // This will return "John Doe":
+//   let x = person.fullName();  
+// console.log(person.fullName())
 
 
